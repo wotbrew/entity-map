d274cf751699376f05d2d508c2bb3fc5076cf3a3
Dan Stone <wotbrew@gmail.com>
2020-06-24 00:07:10 +0100

    A test for the readme example

--- test/com/wotbrew/entity_map_test.clj
+++ test/com/wotbrew/entity_map_test.clj
@@ -3,6 +3,24 @@
   (:require [clojure.test :refer :all]
             [com.wotbrew.entity-map :refer :all]))
 
+(deftest readme-example-test
+  (let [data
+        [{:id 1, :name "foo", :age 10},
+         {:id 2, :name "bar", :age 42},
+         {:id 3, :name "baz", :age 10}]
+
+        em (wrap-coll :id data)
+
+        _ (is (= {1 {:id 1, :name "foo", :age 10},
+                  2 {:id 2, :name "bar", :age 42},
+                  3 {:id 3, :name "baz", :age 10}} em))
+
+        _ (is (= #{1, 3} (eq em :age 10)))
+
+        em2 (assoc em 4 {:name "qux", :age 10})
+
+        _ (is (= #{1, 3, 4} (eq em2 :age 10)))]))
+
 (deftest map-behaviour-test
 
   (is (= {} (entity-map)))
